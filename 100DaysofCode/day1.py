8e6854c917f163d84610144931c4d0429bfde8b0
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-01-01 16:27:10 +0530

    commit

---
+++ 100DaysofCode/day1.py
@@ -0,0 +1,26 @@
+# Exercise-1
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('What to print')")
+
+# Exercise-2
+
+print(len(input()))
+
+# Exercise-3
+
+a = (input("a:"))
+b = (input("b:"))
+c = a
+a = b
+b = c
+print(a)
+print(b)
+
+# Final Project
+
+print("Welcome to Band Name Generator !!! ")
+city = input("In which city you have been grown up?\n")
+pet_name = input("What is your pet name?\n")
+print("Your Band Name is " + city + pet_name)
